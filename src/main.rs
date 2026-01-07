c262fbaed83edf401ceaabe262d06a50e6311701
SanguineTitan57 <69134964+SanguineTitan57@users.noreply.github.com>
2026-01-07 22:10:40 +0300

    feat: Overwritten the entry point
    - Disabled Unwinding
    - Panic Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 // Disable the standard library
-// #![no_std]
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
 
-fn main() {
-    println!("Hello, world!");
+#[panic_handler]
+// ! is the never type, must never return
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
